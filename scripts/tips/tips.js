b008cc58f5a7881e89c3be69d61525bbd08eab63
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-02 15:30:59 -0500

    commented on all my tips js files with my understanding of how they operate

--- scripts/tips/tips.js
+++ scripts/tips/tips.js
@@ -1,3 +1,14 @@
+/* tips.js contains a function "tipsConverter" that take a new parameter "tipsObject"(which is a single tip object) and converts it to an HTML string
+you must put that new string in a container, "tipsHTMLRepresentation"
+...the HTML string is in backticks so you can reference variable with ${}...
+
+we must give out new object (that is passed through as a parameter) a key to ouput "tipsObject.tip" 
+...which displays the "tip" key from tipsCollection when its introduced in tipsList.js
+
+you must then return that container.(so it compiles the function?)
+tipsList.js is dependant on tips.js... tips.js is not dependant on anything
+*/
+
 const tipsConverter = (tipsObject) => {
     const tipsHTMLRepresentation = `<section class="tip">${tipsObject.tip}</section>`
 
--- scripts/tips/tipsData.js
+++ scripts/tips/tipsData.js
@@ -1,3 +1,10 @@
+/* tipsData.js contains a array of tips objects
+-each object has a key:value (string) assoicated with it
+-you will utilise the tipsCollection array inside the tipsList.js where it will be introduced to tips.js in a function.
+...tips.js contains a function that creates a string based on tip keys that it is introduced to in the tipsList.js.. but it needs tipsList to do that, 
+MODULARIZE YOUR CODE! GOTTA KEEP IT SEPERATED
+... tipsList.js is dependent on tipsData.js... tipsData.js is not dependant on anything, at the moment.
+*/
 const tipsCollection = [
     {
         tip: "Poutine viral man braid farm-to-table chambray stumptown."
