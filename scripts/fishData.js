a8d818455c2bd8db2b055f120b77435859d04aa8
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-02 09:57:39 -0500

    removed my fish list html

--- scripts/fishData.js
+++ scripts/fishData.js
@@ -5,6 +5,7 @@ const fishCollection = [
     Name: "Angus",
     Location: "Sea of Google",
     Food: "Popcorn",
+    Picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
     },
 
     { 
@@ -13,6 +14,7 @@ const fishCollection = [
     Name: "Lunch",
     Location: "The Ocean",
     Food: "Lime",
+    Picture: "https://natashaskitchen.com/wp-content/uploads/2017/08/Easy-Fish-Tacos-with-the-Best-Fish-Taco-Sauce-4.jpg"
     },
 
     {
@@ -20,7 +22,8 @@ const fishCollection = [
         Length: 7,
         Name: "Spotted Grumpy Fish",
         Location: "Gulf of Mexico",
-        Food: "Anything Puree'd"
+        Food: "Anything Puree'd",
+        Picture: "https://www.hakaimagazine.com/wp-content/uploads/trout-fish-feel.jpg"
         },
 
     {
@@ -28,7 +31,8 @@ const fishCollection = [
         Length: 8,
         Name: "Fish outta Wata",
         Location: "Homedepot",
-        Food: "Old People"
+        Food: "Old People",
+        Picture: "https://images.homedepot-static.com/productImages/36dc334f-0135-497d-9e79-7fdfd8811506/svn/multicolor-badash-crystal-sculptures-j527-64_600.jpg"
         },
 
     {
@@ -36,6 +40,7 @@ const fishCollection = [
         Length: 3,
         Name: "NoSeaEm",
         Location: "The Purple Ocean Reef",
-        Food: "Highlighters"
-        },
+        Food: "Highlighters",
+        Picture: "https://i.etsystatic.com/7622160/r/il/c0dd51/1276548045/il_570xN.1276548045_jemv.jpg"
+        }
 ]
