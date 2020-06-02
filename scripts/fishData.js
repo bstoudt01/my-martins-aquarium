cad865d925be90fd8bd0a484ebcf25a282c256dd
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-02 09:57:59 -0500

    removed my fish list html2

--- scripts/fishData.js
+++ scripts/fishData.js
@@ -1,46 +1,46 @@
 const fishCollection = [
     {
-    Species: "Parrot Fish",
-    Length: 8,
-    Name: "Angus",
-    Location: "Sea of Google",
-    Food: "Popcorn",
-    Picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+        species: "Parrot Fish",
+        length: 8,
+        name: "Angus",
+        location: "Sea of Google",
+        food: "Popcorn",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
     },
 
     { 
-    Species: "Mahi",
-    Length: 6,
-    Name: "Lunch",
-    Location: "The Ocean",
-    Food: "Lime",
-    Picture: "https://natashaskitchen.com/wp-content/uploads/2017/08/Easy-Fish-Tacos-with-the-Best-Fish-Taco-Sauce-4.jpg"
+        species: "Mahi",
+        length: 6,
+        name: "Lunch",
+        location: "The Ocean",
+        food: "Lime",
+        picture: "https://natashaskitchen.com/wp-content/uploads/2017/08/Easy-Fish-Tacos-with-the-Best-Fish-Taco-Sauce-4.jpg"
     },
 
     {
-        Species: "Spotted Eel",
-        Length: 7,
-        Name: "Spotted Grumpy Fish",
-        Location: "Gulf of Mexico",
-        Food: "Anything Puree'd",
-        Picture: "https://www.hakaimagazine.com/wp-content/uploads/trout-fish-feel.jpg"
-        },
+        species: "Spotted Eel",
+        length: 7,
+        name: "Spotted Grumpy Fish",
+        location: "Gulf of Mexico",
+        food: "Anything Puree'd",
+        picture: "https://www.hakaimagazine.com/wp-content/uploads/trout-fish-feel.jpg"
+    },
 
     {
-        Species: "Rainblow",
-        Length: 8,
-        Name: "Fish outta Wata",
-        Location: "Homedepot",
-        Food: "Old People",
-        Picture: "https://images.homedepot-static.com/productImages/36dc334f-0135-497d-9e79-7fdfd8811506/svn/multicolor-badash-crystal-sculptures-j527-64_600.jpg"
-        },
+        species: "Rainblow",
+        length: 8,
+        name: "Fish outta Wata",
+        location: "Homedepot",
+        food: "Old People",
+        picture: "https://images.homedepot-static.com/productImages/36dc334f-0135-497d-9e79-7fdfd8811506/svn/multicolor-badash-crystal-sculptures-j527-64_600.jpg"
+    },
 
     {
-        Species: "Triggerfish",
-        Length: 3,
-        Name: "NoSeaEm",
-        Location: "The Purple Ocean Reef",
-        Food: "Highlighters",
-        Picture: "https://i.etsystatic.com/7622160/r/il/c0dd51/1276548045/il_570xN.1276548045_jemv.jpg"
-        }
+        species: "Triggerfish",
+        length: 3,
+        name: "NoSeaEm",
+        location: "The Purple Ocean Reef",
+        food: "Highlighters",
+        picture: "https://i.etsystatic.com/7622160/r/il/c0dd51/1276548045/il_570xN.1276548045_jemv.jpg"
+    }
 ]
