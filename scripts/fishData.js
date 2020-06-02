4b309373e51ee70e41d7abdcf2fefaf3ab777afe
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-02 09:30:02 -0500

    created fish and fish list.js and scripts subfolder called fish

--- scripts/fishData.js
+++ scripts/fishData.js
@@ -1,41 +1,41 @@
 const fishCollection = [
-     {
+    {
     Species: "Parrot Fish",
     Length: 8,
     Name: "Angus",
     Location: "Sea of Google",
     Food: "Popcorn",
-    }
+    },
 
-    {
-        Species: "Mahi",
-        Length: 6,
-        Name: "Lunch",
-        Location: "The Ocean",
-        Food: "Lime",
-        }
+    { 
+    Species: "Mahi",
+    Length: 6,
+    Name: "Lunch",
+    Location: "The Ocean",
+    Food: "Lime",
+    },
 
     {
         Species: "Spotted Eel",
         Length: 7,
         Name: "Spotted Grumpy Fish",
         Location: "Gulf of Mexico",
-        Food: "Anything Puree'd",
-        }
+        Food: "Anything Puree'd"
+        },
 
     {
         Species: "Rainblow",
         Length: 8,
         Name: "Fish outta Wata",
         Location: "Homedepot",
-        Food: "Old People",
-        }
+        Food: "Old People"
+        },
 
     {
         Species: "Triggerfish",
         Length: 3,
         Name: "NoSeaEm",
         Location: "The Purple Ocean Reef",
-        Food: "Highlighters",
-        }
+        Food: "Highlighters"
+        },
 ]
