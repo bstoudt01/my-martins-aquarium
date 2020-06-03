4eb8c6cd556aa9ab7fbc40ec14f7eb1bf42c6a72
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-02 19:47:10 -0500

    edited my comments and relooked at my modular javascript to understand it better

--- scripts/tips/tips.js
+++ scripts/tips/tips.js
@@ -1,11 +1,11 @@
 /* tips.js contains a function "tipsConverter" that take a new parameter "tipsObject"(which is a single tip object) and converts it to an HTML string
+use dot notation to inject the key(s) of the tipsData you need by adding it onto the paramater you give tipsConverter.. the parameter name is chosen to best reflect what will be moved through the function.
 you must put that new string in a container, "tipsHTMLRepresentation"
-...the HTML string is in backticks so you can reference variable with ${}...
-
-we must give out new object (that is passed through as a parameter) a key to ouput "tipsObject.tip" 
-...which displays the "tip" key from tipsCollection when its introduced in tipsList.js
+...the HTML string is in backticks so you can have html code and then reference variable with ${}...
 
 you must then return that container.(so it compiles the function?)
+MODULARIZE YOUR CODE! GOTTA KEEP IT SEPERATED 
+This is my Data -> HTML string "converter" Module
 tipsList.js is dependant on tips.js... tips.js is not dependant on anything
 */
 
